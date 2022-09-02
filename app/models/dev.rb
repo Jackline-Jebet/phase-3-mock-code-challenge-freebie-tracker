dccf045cc8a2d2751f137259b5c318652b8fd2a1
Jackline Jebet <jebetjackline2016@gmail.com>
2022-09-02 22:07:19 +0300

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+  
+    def received_one?(item_name)
+      self.freebies.pluck(:item_name).include?(item_name)
+    end
 
+    def give_away(dev, freebie)
+        binding.pry
+        if freebie.dev == self
+          freebie.update(dev_id: dev.id)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
+c1 = Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
+d1 = Dev.create(name: "Rick")
 Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
@@ -17,5 +17,11 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(company_id: c1.id, dev_id: d1.id, item_name: "moon", value: 100000)
+
+items = ["soccer ball", "Stick", "candy", "deck"]
+25.times do 
+  Freebie.create(company_id: Company.all.sample.id, dev_id: Dev.all.sample.id, item_name: items.sample, value: rand(50..1000))
+end
 
 puts "Seeding done!"
